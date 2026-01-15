a918acfed7082517aa0e6a720fa3f1322f4feb72
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-01-15 11:41:08 -0300

    dggh

---
+++ src/Controller/LocalisationController.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Service\Localisation\LocalisationInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\CacheItem;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+/**
+ * Class LocalisationController.
+ */
+ Class LocalisationController
+{
+  /**
+   * @var LoggerInterface
+   */
+  private $logger;
+
+  /**
+   * @var HttpClientInterface
+   */
+  private $client;
+
+  /**
+   * @var CacheInterface
+   */
+  private $cache;
+
+  /**
+   * LocalisationController constructor.
+   *
+   * @param LoggerInterface $logger
+   * @param CacheInterface $cache
+   * @param HttpClientInterface $client
+   */
+  public function __construct(LoggerInterface $logger, CacheInterface $cache, HttpClientInterface $client)
+  {
+      $this->logger = $logger;
+      $this->cache = $cache;
+      $this->client = $client;
+  }
+
+  /**
+   * @Route("/localisation", name="localisation", options={"expose"=true})
+   * @param Request $request
+   *
+   * @return JsonResponse
+   */
+  public function getCurrentLocalisation(Request $request): JsonResponse
+  {
+     $ip = $request->query->get('address');
+
+     /** @var CacheItem $item */
+     $item = $this->cache->getItem($address);
+
+     // On vérifie si l'item cache est toujours valable
+     if (!$item->isHit()) {
+      $url = sprintf('http://nominatim.openstreetmap.org/search/%s?format=%s&polygon=%s&addressdetails=%s', $address, 'json', '1', '1');
+      $response = $this->client->request('GET', $url);
+      $this->logger->info('User localisation', ['provider' => 'ip', 'url' => $url, 'response' => $response]);
+
+      //On stocke la valeur et on ajoute une date d'expiration
+      $item
+          ->set($response->toArray())
+          ->expiresAfter(3600)
+      ;
+      $this->cache->save($item);
+     }
+
+     return new JsonResponse($item->get());
+  }
+}
